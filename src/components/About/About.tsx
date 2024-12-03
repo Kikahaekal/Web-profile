21dd2f1e392b3e638412c7d78749103e151b649e
Kika Haekal <91299160+Kikahaekal@users.noreply.github.com>
2024-12-03 14:51:36 +0700

    fix margin in mobile view

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -4,7 +4,7 @@ import Description from "./Description"
 
 const About = () => {
     return (
-        <div className="text-gray-800 bg-white shadow-2xl rounded-lg p-5 md:p-20">
+        <div className="text-gray-800 bg-white shadow-2xl rounded-lg m-3 md:m-0 p-5 md:p-20">
             <Title text="Tentang Saya"/>
             <Description />
         </div>
